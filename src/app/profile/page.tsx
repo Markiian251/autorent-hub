b78d03ae027082a90ce94ecc919ecb48320783aa
Markiian Somyk <markiiansomyk@MacBook-Air-Markiian.local>
2026-07-03 01:51:14 +0300

    Перший коміт

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+export default function ProfilePage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        Оксана Піська Маленька сіська 
+      </h1>
+    </main>
+  );
+}
