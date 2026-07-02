61165d9ad05c5c30ddacf0f707b3b5c4f1423eb2
Markiian Somyk <markiiansomyk@MacBook-Air-Markiian.local>
2026-07-03 02:44:56 +0300

    update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ export default function LoginPage() {
   return (
     <main className="min-h-screen flex items-center justify-center">
       <h1 className="text-4xl font-bold">
-        Login
+        ХУЙ
       </h1>
     </main>
   );
