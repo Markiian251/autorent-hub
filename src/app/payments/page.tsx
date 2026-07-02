d2285b33aa1c37de709182838a79a95d048f1f4e
Markiian Somyk <markiiansomyk@MacBook-Air-Markiian.local>
2026-07-03 02:09:35 +0300

    Fix pages

---
+++ src/app/payments/page.tsx
@@ -0,0 +1,7 @@
+export default function PaymentsPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Payments</h1>
+    </main>
+  );
+}
